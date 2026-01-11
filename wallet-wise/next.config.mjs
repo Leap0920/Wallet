1947e4ec735f0f07adc54f8214c9b47e32917001
Leap0920 <carlobaclao123@gmail.com>
2026-01-11 15:09:28 +0800

    changes

--- wallet-wise/next.config.mjs
+++ wallet-wise/next.config.mjs
@@ -1,9 +1,9 @@
 import path from 'path';
 
 const nextConfig = {
-  turbopack: {
-    root: path.resolve('./'),
-  },
+    turbopack: {
+        root: path.resolve('./'),
+    },
 };
 
 export default nextConfig;
